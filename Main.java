d71d4365321b06758d436c042f8e03a65893f0c0
bia-programming <bia.pasca@yahoo.com>
2020-10-26 19:53:26 +0200

    Branch development

--- Main.java
+++ Main.java
@@ -2,9 +2,6 @@ package com.company;
 
 import java.util.Scanner;
 
-/*
-O pereche de cate doua numere, se contorizeaza cu +1!
-*/
 public class Main {
 
     public static void main(String[] args) {
